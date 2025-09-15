152cd2a2f19b6dd40559d60aa85b60b9dfc266ff
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-09-15 23:28:51 +0300

    created profile.class.php, Profile management for Ticketbimport plugin

---
+++ inc/profile.class.php
@@ -0,0 +1,101 @@
+<?php
+/**
+ * Profile management for Ticket Import plugin
+ */
+
+if (!defined('GLPI_ROOT')) {
+    die("Sorry. You can't access this file directly");
+}
+
+class PluginImportticketsProfile extends Profile {
+    
+    static $rightname = "plugin_importtickets";
+    
+    /**
+     * Get tab name for item
+     */
+    function getTabNameForItem(CommonGLPI $item, $withtemplate = 0) {
+        if ($item->getType() == 'Profile') {
+            return __('Ticket Import', 'importtickets');
+        }
+        return '';
+    }
+    
+    /**
+     * Display tab content
+     */
+    static function displayTabContentForItem(CommonGLPI $item, $tabnum = 1, $withtemplate = 0) {
+        if ($item->getType() == 'Profile') {
+            $profile = new self();
+            $profile->showForm($item->getID());
+        }
+        return true;
+    }
+    
+    /**
+     * Show profile form
+     */
+    function showForm($profiles_id = 0, $openform = true, $closeform = true) {
+        $canedit = Session::haveRightsOr(self::$rightname, [CREATE, UPDATE, PURGE]);
+        if ($canedit && $openform) {
+            echo "<form method='post' action='" . $this->getFormURL() . "'>";
+        }
+        
+        $profile = new Profile();
+        $profile->getFromDB($profiles_id);
+        
+        $rights = $this->getAllRights();
+        $profile->displayRightsChoiceMatrix($rights, [
+            'canedit'       => $canedit,
+            'default_class' => 'tab_bg_2',
+            'title'         => __('General rights')
+        ]);
+        
+        if ($canedit && $closeform) {
+            echo "<div class='center'>";
+            echo Html::hidden('id', ['value' => $profiles_id]);
+            echo Html::submit(_sx('button', 'Save'), ['name' => 'update']);
+            echo "</div>\n";
+            Html::closeForm();
+        }
+    }
+    
+    /**
+     * Get all rights
+     */
+    static function getAllRights() {
+        $rights = [
+            [
+                'itemtype' => 'PluginImporttickets',
+                'label'    => __('Use ticket import feature', 'importtickets'),
+                'field'    => 'plugin_importtickets'
+            ]
+        ];
+        
+        return $rights;
+    }
+    
+    /**
+     * Install profiles
+     */
+    static function install($migration) {
+        global $DB;
+        
+        $profileRight = new ProfileRight();
+        foreach (self::getAllRights() as $right) {
+            if (!countElementsInTable('glpi_profilerights', ['name' => $right['field']])) {
+                $profileRight->add(['name'  => $right['field'], 'rights' => 0]);
+            }
+        }
+    }
+    
+    /**
+     * Uninstall profiles
+     */
+    static function uninstall() {
+        $profileRight = new ProfileRight();
+        foreach (self::getAllRights() as $right) {
+            $profileRight->deleteByCriteria(['name' => $right['field']]);
+        }
+    }
+}
