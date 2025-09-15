53fe97f9134e0f54332e88063894cf0b3a7ba487
Bill Graham Peacemaker <grahambill011@gmail.com>
2025-09-16 00:44:21 +0300

    Debugging

--- hook.php
+++ hook.php
@@ -3,8 +3,8 @@
  * Plugin hooks for Ticket Import
  */
 
-function plugin_importtickets_Menu() {
-    if (class_exists('PluginImporttickets')) {
+function plugin_importtickets_menu_entry() {
+    if (class_exists('PluginImporttickets') && Session::haveRight('plugin_importtickets', READ)) {
         return PluginImporttickets::getMenuContent();
     }
     return [];
